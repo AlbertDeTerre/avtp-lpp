75b3d79de9b5c24822fce3010f7b18f29b5814b7
timplass <tplassch@bagacum1728.fr>
2022-12-16 17:43:44 +0100

    ok

--- tim.c
+++ tim.c
@@ -29,13 +29,12 @@ typedef struct commande{
 	int id,nbplat,nbboisson; 	//id de la commande,nombre de plat, nombre de boisson
 	table table;				//table concernée
 	float prixtotal;			//prix total de la commande
-	boisson boisson[20]; 		//boisson commandée
-	plat plat[18];
+	struct boisson *boisson[20]; 		//boisson commandée
+	struct plat *plat[18];
 	struct commande *suivante;
 	
 					//plat commandé
 }commande;
-
 
 
 typedef struct ticket{
@@ -45,9 +44,24 @@ typedef struct ticket{
 }ticket;
 
 
+void ecriturelisteBoi(boisson *bcourant){
+	int i=0;
+	boisson *bdeb, *bsuivant;
+	
+	while (i<=sizeof(boisson)){       
+        printf("%2d %s %5.2f\n",bcourant->id, bcourant->nom,bcourant->prix);
+        
+        bcourant = bcourant->suivant;
+        i++;
+        
+    }
+}
+
+
+
 boisson * lectureBoisson(){
 	//ouverture des fichiers
-	
+	int i;
 	FILE *fboisson;
 	fboisson = fopen("VoiturierPlasschaertBoisson.dat","r");
 	boisson *bdeb, *bcourant, *bsuivant;
@@ -70,10 +84,11 @@ boisson * lectureBoisson(){
         fscanf(fboisson, "%2d", &bcourant->id);
         
     }
-    //affectation de la premiere structure de la liste Ã  bcourant
     bcourant = bdeb;
+    //affectation de la premiere structure de la liste Ã  bcourant
+    
 
-    return bcourant;
+    return bdeb;
 	
 	
 	
@@ -92,33 +107,29 @@ plat * lecturePlat(){
     //Lecture du fichier et affectation des differentes valeurs lues dans les variables de la structure table
     pcourant = malloc(sizeof(plat));
     pdeb = pcourant;
-    
-    
     fscanf(fplat, "%2d", &pcourant->id);
     
     
     //Commencement de la boucle de lecture
     while (!feof(fplat)){       
         fscanf(fplat, "%s %f", &pcourant->nom,&pcourant->prix);
-        
         psuivant = malloc(sizeof(plat));
         pcourant->suivant = psuivant;
         pcourant = psuivant;
         fscanf(fplat, "%2d", &pcourant->id);
-        
     }
     //affectation de la premiere structure de la liste Ã  bcourant
     pcourant = pdeb;
-
     return pcourant;
 	
-	
-	
 }
+
+
 
 commande *faireCommande(){
 	//declaration des variables 
 	int contu,id,tab;
+	void ajoutart(int, int,int);
 	
 	//Demande de la table concernée
 	printf("Entrez la table concernée:\n");
@@ -129,25 +140,37 @@ commande *faireCommande(){
 		//demande des données du plats/boisson
 		printf("Entrez 1 pour les plats, 2 pour les boissons\n");
 		scanf("%2d",&contu);
-		
-		//id du plat
-		printf("Entrez l'id:");
-		scanf("%2d",&id);
-		//ajout de l'article dans la commande
-		ajoutart(contu,id,tab);
+		if((contu>=1)){
+			//id du plat
+			printf("Entrez l'id:");
+			scanf("%2d",&id);
+			
+			
+			
+				//ajout de l'article dans la commande
+			ajoutart(contu,id,tab);
+			
+			
+		}
 		}
 		//fin
 		printf("Commande finie.");
-			
+		
 }
 
+
+
+
+
 void ajoutart(int type, int id, int tab){
+
 	//decla variable;
-	boisson *bcourant,*bdeb;
+	boisson *bcourant,*bdeb,*bsuivant;
 	bcourant = lectureBoisson();
-	plat *pcourant, *pdeb;
+	plat *pcourant, *pdeb,*psuivant;
 	pcourant = lecturePlat();
-	commande *ccourant, cdeb;
+	commande *ccourant, *cdeb,*csuivant;
+	
 	
 	
 	//creation de la commande
@@ -157,47 +180,80 @@ void ajoutart(int type, int id, int tab){
     ccourant->id = tab;
     ccourant->nbboisson =0;
     ccourant->nbplat = 0;
-    
-    
-    
+    printf("%2d ",sizeof(plat));
     //ajout du plat
     if(type == 1){
-    	
+    	//verification que le plat existe
+    	if(id < sizeof(plat)){
+    	//boucle qui recherche le bon plat.
     	while(id != pcourant->id){
-    		pcourant->suivant = psuivant;
-        	pcourant = psuivant;
+    		
+        	pcourant = pcourant->suivant;
+        	printf("%2d",pcourant->id);
 		}
+		//ajout du plat dans la commande
 		ccourant->nbplat +=1;
-		ccourant->plat[nbplat] = pcourant->plat;
-		
+		ccourant->plat[ccourant->nbplat] = pcourant;
 		
 	}
-	if(type == 2){
-    	
-    	while(id != bcourant->id){
-    		bcourant->suivant = bsuivant;
-        	bcourant = bsuivant;
+	else{
+		printf("Ce plat n'existe pas.");
+	}
+	}
+	else if(type == 2){
+		if(id <= sizeof(boisson)){
+		
+	    	while(id != bcourant->id){
+	    		
+	        	bcourant = bcourant->suivant;
+			}
+			ccourant->nbboisson +=1;
+			ccourant->boisson[ccourant->nbboisson] = bcourant;
 		}
-		ccourant->nbboisson +=1;
-		ccourant->boisson[nbboisson] = bcourant->boisson;
-		
-		
-		
+		else{
+			printf("Cette boisson n'existe pas");
+		}
+	}
+	else{
+		printf("erreur\n");
 	}
     
     
-    
-
-	
-	
-	
 }
-void faireTicket(commande []commande){
-	FILE*fdirect, fsoiree;
+
+
+
+
+
+
+
+void faireTicket(int tab){
+	FILE*fdirect, *fcaisse;
 	fdirect = fopen("VoiturierPlasschaertTicketCli.res","w");
 	fcaisse = fopen("VoiturierPlasschaertTicketCaisse.res","a");
+	boisson *bcourant,*bdeb,*bsuivant;
+	bcourant = lectureBoisson();
+	plat *pcourant, *pdeb,*psuivant;
+	pcourant = lecturePlat();
+	commande *ccourant, *cdeb,*csuivant;
+	//Fonction qui affiche la date et l'heure du ticket
+	time_t timer;
+    char buffer[26];
+    struct tm* tm_info;
+    timer = time(NULL);
+    tm_info = localtime(&timer);
+	strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+	fprintf(fdirect,"      %s ",buffer);
 	
-	
+	bcourant = bdeb;
+	while (sizeof(boisson)){
+	       
+        printf("%2d %s %f",bcourant->id,bcourant->nom,bcourant->prix);
+        
+        bcourant->suivant = bsuivant;
+        bcourant = bsuivant;
+        ;
+    }
 }
 
 
@@ -207,16 +263,20 @@ void faireTicket(commande []commande){
 
 
 main(){
-	
-	
-	int i;
-	boisson *bcourant,*bdeb;
-	
+	boisson *bcourant,*bdeb,*bsuivant;
 	bcourant = lectureBoisson();
-	plat *pcourant,*pdeb;
-	pcourant = lecturePlat(); 
+	plat *pcourant, *pdeb,*psuivant;
+	pcourant = lecturePlat();
+	commande *ccourant, *cdeb,*csuivant;
+	
+	//ecriturelisteBoi(bcourant);
+	
+	
 	faireCommande();
-    
+	faireTicket(1);
+	
+	//TODO faire id des plats 
+	
     
     
     
