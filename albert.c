d62d176eebaaf8badd52435bdf09c9896a4b2ae6
AlbertDeTerre <albert@voiturier.net>
2022-12-17 21:55:56 +0100

    Reservation et Annulation de reservation FINI

--- albert.c
+++ albert.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <time.h>
 
 //Structure table
 typedef struct table{
@@ -24,8 +25,8 @@ int main()
     FILE *ftab, *fmenu, *fboi, *fpersonnel, *fhoraire;
     
     //Déclaration des fonctions et variables;
-    int running = 1, identification();
-    void reservation(table*);
+    int running = 1, identification(), jourSemaine(int, int, int);
+    void reservation(table*), annulerReservation();
     table *tdeb, *tcourant;
     personnel *pdeb, *pcourant;
     table * lectureTable();
@@ -46,7 +47,13 @@ int main()
 
         //L'utilisateur veut encoder une réservation
         if(option == 1){
-            reservation(tcourant);
+            printf("1: AJOUTER UNE RESERVATION | 2: ANNULER UNE RESERVATION | AUTRE: QUITTER\n");
+            scanf("%2d", &option);
+            if (option == 1){
+                reservation(tcourant);
+            }else if (option == 2){
+                annulerReservation();
+            }
 
         //L'utilisateur veut encoder une commande
         }else if(option == 2){
@@ -57,7 +64,7 @@ int main()
             break;
         }
 
-        running = 0;
+        
     }
 
     printf("\n** FIN DU PROGRAMME **\n");
@@ -120,6 +127,7 @@ personnel * lecturePersonnel(){
         fscanf(fpers, "%3d", &mat);
     }
     courant = NULL;
+    fclose(fpers);
     return deb;
 }
 
@@ -149,6 +157,7 @@ int identification(){
 
     //Si le test entre les deux matricule échoue
     printf("\n!! Identification échouée !!\n");
+    fclose(fdat);
     return 0;
 }
 
@@ -157,7 +166,6 @@ void reservation(table * courant){
     //Ouverture des fichiers nécéssaires
     FILE *fres, *fdat, *fjours;
     fjours = fopen("VoiturierPlasschaertJours.dat", "r");
-    fres = fopen("VoiturierPlasschaertReservations.res", "a");
     fdat = fopen("VoiturierPlasschaertReservations.res", "r");
     table *deb, *tableChoisie, *table;
     int reservations[8][50], existe = 0;
@@ -167,7 +175,7 @@ void reservation(table * courant){
     int nbPersonnes = 999999, numTableChoisie = 0, min = 999999, tablesRes[50], nbTablesRes, resReussie = 0;
     char jour[10], jourFichier[10];
 
-    //Demande à l'utilisateur pour combien de personne le client à reserver
+    //Demande à l'utilisateur quel est le jour de la réservation
     printf("\nJour de la réservation: ");
     scanf("%9s", jour);
 
@@ -224,18 +232,23 @@ void reservation(table * courant){
     printf("\nNombre de personnes: ");
     scanf("%2d", &nbPersonnes);
 
+    int nbTablesResEcriture[8], tablesResEcriture[8][50];
+
     //Initialisation d'un liste des tables déjà réservées le jour voulu
     for (int i = 1; i <= 7; i++){
         fscanf(fdat, "%9s", jours[i]);
-
         if (strcmp(jours[i], jour) == 0){
             fscanf(fdat, "%2d", &nbTablesRes);
+            nbTablesResEcriture[i] = nbTablesRes;
             for (int j = 0; j < nbTablesRes; j++){
                 fscanf(fdat, "%2d", &tablesRes[j]);
+                tablesResEcriture[i][j] = tablesRes[j];
             }
-            break;
         }else{
-            fscanf(fdat, "%*[^\n]\n");
+            fscanf(fdat, "%2d", &nbTablesResEcriture[i]);
+            for (int j = 0; j < nbTablesRes; j++){
+                fscanf(fdat, "%2d", &tablesResEcriture[i][j]);
+            }
         }
     }
 
@@ -260,6 +273,141 @@ void reservation(table * courant){
         courant = courant->suivant;
     }
 
-    
+    char tmp[10];
+
+    //Si la table peut bien être réservée
+    if(numTableChoisie != 0){
+        for (int i = 1; i <= 7; i++){
+            printf("%-9s %02d ", jours[i], nbTablesResEcriture[i]);
+            for (int j = 0; j < nbTablesResEcriture[i]; j++){
+                printf("%02d ", tablesResEcriture[i][j]);
+            }
+            printf("\n");
+        }
+
+        fclose(fdat);
+        fres = fopen("VoiturierPlasschaertReservations.res", "w");
+
+        for (int i = 1; i <= 7; i++){
+            if(strcmp(jour, jours[i]) == 0){
+
+                fprintf(fres, "%-9s %02d ", jours[i], nbTablesResEcriture[i] + 1);
+            }else{
+                fprintf(fres, "%-9s %02d ", jours[i], nbTablesResEcriture[i]);
+            }
+
+            for (int j = 0; j < nbTablesResEcriture[i]; j++){
+                fprintf(fres, "%02d ", tablesResEcriture[i][j]);
+                if (strcmp(jour, jours[i]) == 0 && j == nbTablesResEcriture[i] - 1){
+                    fprintf(fres, "%02d", numTableChoisie);
+                }
+            }
+            fprintf(fres, "\n");
+        }
+
+    }
+
+    fclose(fres);
+
+}
+
+int jourSemaine(int jour, int mois, int annee){
+    int joursemaine = (jour += mois < 3 ? annee-- : annee - 2, 23*mois/9 + jour + 4 + annee/4- annee/100 + annee/400)%7; 
+    if (joursemaine == 0){
+        joursemaine = 7; 
+    }
+    return joursemaine;
+}
+
+void annulerReservation(){
+    char tmp[10], jour[10], jours[8][10];
+    int existe = 0, tablesRes[8][50], nbTablesRes[8];
+
+    FILE *fjours, *freservR, *freservW;
+    fjours = fopen("VoiturierPlasschaertJours.dat", "r");
+    freservR = fopen("VoiturierPlasschaertReservations.res", "r");
+
+    printf("Jour de la réservation à annuler: ");
+    scanf("%9s", jour);
+
+    //On met toutes les lettre en majuscule
+    for (int i = 0; i < 10; i++){
+        if (jour[i] == '\0'){
+            break;
+        }
+        jour[i] = toupper(jour[i]);
+    }
+
+    //Check si le jour existe
+    for (int i = 1; i <= 7; i++){
+        fscanf(fjours, "%9s", jours[i]);
+
+        if (strcmp(jour, jours[i]) == 0){
+            existe = i;
+        }
+    }
+
+    //Redemande le jour jusqu'a ce qu'il soit bon
+    while (existe == 0){
+        printf("\n!! CE JOUR N'EXISTE PAS !!\nJour de la réservation à annuler: ");
+        scanf("%9s", jour);
+
+        for (int i = 0; i < 10; i++){
+            if (jour[i] == '\0'){
+                break;
+            }
+            jour[i] = toupper(jour[i]);
+        }
+
+        for (int i = 1; i <= 7; i++){
+            fscanf(fjours, "%9s", jours[i]);
+            
+            if (strcmp(jour, jours[i]) == 0){
+                existe = i;
+            }
+        }
+    }
+
+    int numTableAnnuler = 0, nvNbTables = 0;
+
+    printf("\nNuméro de la table à annuler: ");
+    scanf("%2d", &numTableAnnuler);
+
+    //Initialisation du vecteur avec toutes les tables reservées
+    for (int i = 1; i <= 7; i++){
+        fscanf(freservR, "%9s %2d", tmp, &nbTablesRes[i]);
+        printf("%-9s %2d ", jours[i], nbTablesRes[i]);
+        for (int j = 0; j < nbTablesRes[i]; j++){
+            fscanf(freservR, "%2d", &tablesRes[i][j]);
+            printf("%2d ", tablesRes[i][j]);
+            if (strcmp(jours[i], jour) == 0 && tablesRes[i][j] == numTableAnnuler){
+                nvNbTables = nbTablesRes[i] - 1;
+            }
+        }
+        printf("\n");
+    }
+
+    //Fermeture du fichier de lecture
+    fclose(freservR);
+
+    freservW = fopen("VoiturierPlasschaertReservations.res", "w");
+    //Réécriture dans le fichier réservations
+    for (int i = 1; i <= 7; i++){
+
+        //Si le jour = celui choisi et que le numéro de table à annuler existe, alors on ecrit le nouveau nombre de table
+        if (strcmp(jours[i], jour) == 0 && numTableAnnuler != 0){
+            fprintf(freservW, "%-9s %02d ", jours[i], nvNbTables);
+        }else{
+            fprintf(freservW, "%-9s %02d ", jours[i], nbTablesRes[i]);
+        }
+        for (int j = 0; j < nbTablesRes[i]; j++){
+            if (!(strcmp(jours[i], jour) == 0 && tablesRes[i][j] == numTableAnnuler)){
+                fprintf(freservW, "%02d ", tablesRes[i][j]);
+            }
+        }
+        fprintf(freservW, "\n");
+    }
+
+    fclose(freservW);
 
 }
